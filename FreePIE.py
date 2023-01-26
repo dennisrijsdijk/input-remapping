ceae86bee97b91a6a588f58de20c8fb263ad1ef2
Dennis <70665154+dennisrijsdijk@users.noreply.github.com>
2023-01-26 21:15:52 +0100

    Comments & Variable names pt. 1

--- FreePIE.py
+++ FreePIE.py
@@ -1,19 +1,25 @@
 from time import sleep
 
+# Note to mouse definition.
 NOTE_TO_MOUSE = {
-	(0, 34): "mouseLR", # tuple (channel, buffer[0]): "actionAsString"
+	# (MIDI_CHANNEL, DATA_0): "ACTION",
+	(0, 34): "mouseLR",
 	(1, 34): "mouseUD",
 	(1, 1): "rightMouse",
 	(1, 0): "leftMouse",
 }
 
-WHEEL_0_RIGHT = 65
-WHEEL_0_LEFT = 63
+DECK_MOD_CONST = 64 # Scratch Deck Modifier Constant.
 
+# Keys that follow NOTE_DOWN and NOTE_UP behavior
+# or have a value range.
+# CONDITIONAL_LAMBDA should compare DATA_1 with a known value
+# and return True if the key should be pressed.
 NOTE_TO_KEY = {
+	# (MIDI_CHANNEL, DATA_0): (KEY, CONDITIONAL LAMBDA)
 	(4, 17): (Key.W, lambda a : a >= 20),
 	(4, 18): (Key.S, lambda a : a >= 20),
-	(4, 11): [
+	(4, 11): [ # Multi-action
 		(Key.A, lambda a : a <= 15),
 		(Key.D, lambda a : a >= 112),
 	],
@@ -47,16 +53,23 @@ NOTE_TO_KEY = {
 	(1,10): (Key.NumberPad0, lambda a : a == 127)
 }
 
-NOTE_TO_KEY_UPPIES = {
+# Keys that need to be released after being triggered.
+NOTE_TO_KEY_RELEASE = {
+	# (MIDI_CHANNEL, DATA_0): KEY,
 	(4, 45): Key.E,
 }
 
-NOTE_TO_CUE = {
+# Keys that need to latch during NOTE_DOWN, unlatching on the next NOTE_DOWN event.
+# Expects the DATA_1 to == 127 to latch.
+NOTE_TO_KEY_LATCH = {
+	# (MIDI_CHANNEL, DATA_0): KEY,
 	(4, 70): Key.LeftShift,
 	(4, 74): Key.Q
 }
 
+# Called each time a MIDI event comes through
 def update():
+	# Begin Debug
 	diagnostics.watch(midi[0].data.channel)
 	diagnostics.watch(midi[0].data.status)
 	diagnostics.watch(midi[0].data.buffer[0]) # Control Num
@@ -70,20 +83,21 @@ def update():
 	diagnostics.debug("control value:")
 	diagnostics.debug(midi[0].data.buffer[1])
 	diagnostics.debug("")
+	# End Debug
 	action = NOTE_TO_MOUSE.get((midi[0].data.channel, midi[0].data.buffer[0]), "NONE")
 	if (action != "NONE"):
 		diagnostics.debug(action)
 		diagnostics.debug("")
 		if (action == "mouseLR"):
-			if (midi[0].data.buffer[1] > 65): # If going right
-				mouse.deltaX = (midi[0].data.buffer[1] - 65) / 3.2
+			if (midi[0].data.buffer[1] > DECK_MOD_CONST): # If going right
+				mouse.deltaX = (midi[0].data.buffer[1] - DECK_MOD_CONST) / 3.2
 			else:
-				mouse.deltaX = (-65 + midi[0].data.buffer[1]) / 3.2
+				mouse.deltaX = (-DECK_MOD_CONST + midi[0].data.buffer[1]) / 3.2
 		elif (action == "mouseUD"):
-			if (midi[0].data.buffer[1] > 65): # If going right
-				mouse.deltaY = (midi[0].data.buffer[1] - 65) / -3.2
+			if (midi[0].data.buffer[1] > DECK_MOD_CONST): # If going right
+				mouse.deltaY = (midi[0].data.buffer[1] - DECK_MOD_CONST) / -3.2
 			else:
-				mouse.deltaY = (-65 + midi[0].data.buffer[1]) / -3.2
+				mouse.deltaY = (-DECK_MOD_CONST + midi[0].data.buffer[1]) / -3.2
 		if (action == "leftMouse"):
 			mouse.leftButton = midi[0].data.buffer[1] == 127
 		elif (action == "rightMouse"):
@@ -97,12 +111,12 @@ def update():
 			else:
 				if (key == (midi[0].data.channel, midi[0].data.buffer[0])):
 					keyboard.setKey(value[0], value[1](midi[0].data.buffer[1]))
-		action = NOTE_TO_KEY_UPPIES.get((midi[0].data.channel, midi[0].data.buffer[0]), "NONE")
+		action = NOTE_TO_KEY_RELEASE.get((midi[0].data.channel, midi[0].data.buffer[0]), "NONE")
 		if (action != "NONE"):
 			keyboard.setKey(action, True)
-			sleep(0.01)
+			sleep(0.01) # Framerate-dependent on some games, may need tweaking.
 			keyboard.setKey(action, False)
-		action = NOTE_TO_CUE.get((midi[0].data.channel, midi[0].data.buffer[0]), "NONE")
+		action = NOTE_TO_KEY_LATCH.get((midi[0].data.channel, midi[0].data.buffer[0]), "NONE")
 		if (action != "NONE"):
 			if (midi[0].data.buffer[1] == 127):
 				if (keyboard.getKeyDown(action)):
@@ -114,8 +128,9 @@ def update():
 if starting:
 	midi[0].update += update
 
-if keyboard.getKeyDown(Key.Grave): 
+# Emergency Killswitch
+if keyboard.getKeyDown(Key.Grave): # Choose a key that isn't used elsewhere like `
 	mouse.deltaX = 0
 	mouse.deltaY = 0
-	keyboard.setKeyUp(Key.W)
+	keyboard.setKeyUp(Key.W) # Should add all possible keys here.
 	exit()	
